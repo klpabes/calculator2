90d5ea6b09ea60674ceafb3d43f40a3679409a07
klpabes <121295353+klpabes@users.noreply.github.com>
2025-02-10 21:33:09 +0800

    Add querySelector to all buttons Add variables for operate function, num1, num2 and operation Add eventlistener to all btn-numbers to display in calculator-display with 12 character limit Add eventlistener to all btn-operators Add eventlistener to btn-equals that uses operate function given a num1, num2 and an operators Add eventlistener to btn-clear that clears the calculator Add eventlistener to btn-period that adds decimal to calculator display if there's no decimal Add eventlistener to btn-percent that makes innerText of calculator-display into percentage Add keyboard support for numbers and backspace

--- javascript.js
+++ javascript.js
@@ -1,5 +1,74 @@
 'use strict'   
 
+const btnNums = document.querySelectorAll('.btn-number');
+const display = document.querySelector('.calculator-display');
+const btnOperate = document.querySelector('#btn-equals');
+const btnOperators = document.querySelectorAll('.btn-operator');
+const btnClear = document.querySelector('#btn-clear');
+const btnPeriod = document.querySelector('#btn-period');
+const btnSign = document.querySelector('#btn-sign');
+const btnPercent = document.querySelector('#btn-percent');
+
+let num1, num2, operation, sum;
+
+btnNums.forEach(number => {
+    number.addEventListener("click", (e) => {
+            if (display.innerText.length <= 12) display.innerText += number.innerText;
+    })
+});
+
+btnOperators.forEach(operator => {
+    operator.addEventListener("click", (e) => {
+        if (display.textContent) {
+            num1 = display.textContent;
+            display.textContent = '';
+            operation = operator.textContent;
+        }
+
+    })
+})
+
+btnOperate.addEventListener("click", (e) => {
+    if(num1 && operation && display.textContent) {
+        num2 = display.textContent;
+        display.textContent = operate(+num1, +num2, operation);
+    }
+})
+
+btnClear.addEventListener('click', (e) => {
+    num1 = '';
+    num2 = '';
+    operation = '';
+    display.textContent = '';
+})
+
+btnPeriod.addEventListener('click', (e) => {
+    if (display.textContent && !display.textContent.includes('.')) {
+        display.textContent += '.';
+    }
+})
+
+btnSign.addEventListener('click', (e) => {
+    if(display.innerText) display.innerText = display.innerText * -1; 
+})
+
+btnPercent.addEventListener('click', (e) => {
+    if(display.innerText) display.innerText = (display.innerText/100).toString();
+})
+
+document.addEventListener('keydown', (e) => {
+    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    if (numbers.indexOf(e.key)!=-1) {
+        display.textContent += e.key;
+    }
+})
+
+document.addEventListener('keydown', (e) => {
+   if (e.key === 'Backspace') {
+    display.textContent = display.textContent.slice(0, -1);
+   }
+})
+
 function add (a, b) {
     return a + b;
 };
@@ -13,6 +82,7 @@ function multiply (a, b) {
 }
 
 function divide (a, b) {
+    if (b === 0) return 'ERROR';
     return a / b;
 }
 
